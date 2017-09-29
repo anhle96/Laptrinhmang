830794de374542c58e465e34394a127cf2a257b3
anhle96 <lequocanh96@gmail.com>
2017-09-29 11:31:16 +0700

    Them bai

---
+++ LeQuocAnh-b1/Bai3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LeQuocAnh-b1/LeQuocAnh-b1/Bai1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+namespace LeQuocAnh_b1
+{
+    class GetIP
+    {
+        static void Main(string[] args)
+        {
+            String strHostName = string.Empty;
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
