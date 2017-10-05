664bb4038f69348d856a5d7847c4691e22708129
unknown <lequocanh96@gmail.com>
2017-10-05 09:35:59 +0700

    cap nhat cau 3

--- LeQuocAnh-b1/Bai3/Program.cs
+++ LeQuocAnh-b1/Bai3/Program.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Bai3
+namespace BAI03
 {
     class Program
     {
+        private static string strIP;		//input from user
+        private static int inpMask;	//input from user
+
+        private static List<int> ip;
+        private static List<int> mask;
+        private static List<int> netID;
+        private static List<int> broadcast;
+
+        private static void input()
+        {
+            Console.Write("Nhap dia chi IP: ");
+            strIP = Console.ReadLine();
+            Console.Write("Nhap dia chi broadcast: ");
+            inpMask = Convert.ToInt32(Console.ReadLine());
+        }
         static void Main(string[] args)
         {
+            input();
         }
     }
 }
