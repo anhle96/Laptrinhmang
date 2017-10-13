0ca1eb201eef5bad48254c028e5d5b6681f10863
unknown <lequocanh96@gmail.com>
2017-10-13 11:05:58 +0700

    Update Oantuxi_server

--- LeQuocAnh-b3/Oantuxi_Client/Oantuxi_Server/Program.cs
+++ LeQuocAnh-b3/Oantuxi_Client/Oantuxi_Server/Program.cs
@@ -13,11 +13,48 @@ namespace Oantuxi_Server
     {
         static void Main(string[] args)
         {
-            IPEndPoint ipServer = new IPEndPoint(IPAddress.Any, 12345);
-            UdpClient server = new UdpClient(ipServer);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ipep);
+            Console.WriteLine("Waiting for a client...");
 
-            IPEndPoint ipClient = new IPEndPoint(IPAddress.Any, 0);
-            byte[] rec = server.Receive(ref ipClient);
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+
+            byte[] data = new byte[4];
+            server.ReceiveFrom(data, ref Remote);
+            int clientChoosen = BitConverter.ToInt32(data, 0);
+            Random rand = new Random();
+            int serverChoosen = rand.Next(0, 2);
+            if (clientChoosen == serverChoosen)
+            {
+                byte[] gui = Encoding.ASCII.GetBytes("Hoa");
+                server.SendTo(gui, Remote);
+            }
+            if (clientChoosen == 0 && serverChoosen == 1)
+            {
+                byte[] gui = Encoding.ASCII.GetBytes("Thua");
+            }
+            if (clientChoosen == 0 && serverChoosen == 2)
+            {
+                byte[] gui = Encoding.ASCII.GetBytes("Thang");
+            }
+            if (clientChoosen == 1 && serverChoosen == 0)
+            {
+                byte[] gui = Encoding.ASCII.GetBytes("Thang");
+            }
+            if (clientChoosen == 1 && serverChoosen == 2)
+            {
+                byte[] gui = Encoding.ASCII.GetBytes("Thua");
+            }
+            if (clientChoosen == 2 && serverChoosen == 0)
+            {
+                byte[] gui = Encoding.ASCII.GetBytes("Thua");
+            }
+            if (clientChoosen == 2 && serverChoosen == 1)
+            {
+                byte[] gui = Encoding.ASCII.GetBytes("Thang");
+            }
         }
     }
 }
