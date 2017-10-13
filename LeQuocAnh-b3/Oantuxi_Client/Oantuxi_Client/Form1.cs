49d7b61fdd33ed95bc3e759a80b673dfa7f3b6e6
unknown <lequocanh96@gmail.com>
2017-10-13 11:49:05 +0700

    Update Oantuxi_client

--- LeQuocAnh-b3/Oantuxi_Client/Oantuxi_Client/Form1.cs
+++ LeQuocAnh-b3/Oantuxi_Client/Oantuxi_Client/Form1.cs
@@ -14,6 +14,9 @@ namespace Oantuxi_Client
 {
     public partial class Form1 : Form
     {
+        Socket client;
+        IPEndPoint ipep;
+        EndPoint remote;
         public Form1()
         {
             InitializeComponent();
@@ -21,10 +24,26 @@ namespace Oantuxi_Client
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            remote = (EndPoint)ipep;
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Keo";
+            int a = 0;
+            byte[] gui = BitConverter.GetBytes(a);
+            client.SendTo(gui, remote);
+
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipep;
+            client.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
