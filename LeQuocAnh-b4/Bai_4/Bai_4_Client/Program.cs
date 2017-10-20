3f7cbaa260f335ff4cd5fd137e84f8f24e120fcf
unknown <lequocanh96@gmail.com>
2017-10-20 11:31:11 +0700

    Upadte bài 4

---
+++ LeQuocAnh-b4/Bai_4/Bai_4_Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace Bai_4_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            String input, stringData;
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            TcpClient server;
+            try
+            {
+                server = new TcpClient();
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Unable to connect to server!");
+                return;
+            }
+            server.Connect(ip);
+            NetworkStream ns = server.GetStream();
+
+            int recv = ns.Read(data, 0, data.Length);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            if (stringData == "shutdown")
+            {
+                Process.Start("shutdown", "/s /t 0");
+            }
+            if (stringData == "restart")
+            {
+                Process.Start("shutdown", "/r /t 0");
+            }
+        }
+    }
+}
---
+++ LeQuocAnh-b4/Bai_4/Bai_4_Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai_4_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            byte[] data = new byte[1024];
+            TcpListener server = new TcpListener(9050);
+            server.Start();
+           
+            Console.WriteLine("Waitting for Client....");
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+
+            Console.WriteLine("Chon lenh:");
+            string cmd = Console.ReadLine();
+            data = Encoding.ASCII.GetBytes(cmd);
+            ns.Write(data, 0, data.Length);
+        }
+    }
+}
