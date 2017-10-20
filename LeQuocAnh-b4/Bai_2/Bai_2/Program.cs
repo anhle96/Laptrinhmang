e499f2c3ed5bac89e1b3b6327b5b6b22b7b14767
unknown <lequocanh96@gmail.com>
2017-10-20 10:43:50 +0700

    Upadte bài 2

--- LeQuocAnh-b4/Bai_2/Bai_2/Program.cs
+++ LeQuocAnh-b4/Bai_2/Bai_2/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace Bai_2
 {
@@ -10,7 +13,37 @@ namespace Bai_2
     {
         static void Main(string[] args)
         {
+            string IpAddress;
+            int port;
+            string protocol;
+            Console.WriteLine("Nhap Host IP:");
+            IpAddress = Console.ReadLine();
+            Console.WriteLine("Nhap Host Port");
+            port = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap Protocol:");
+            protocol = Console.ReadLine();
 
+            switch (protocol)
+            {
+                case "TCP":
+                    IPEndPoint ip = new IPEndPoint(IPAddress.Parse(IpAddress), port);
+                    TcpClient client = new TcpClient();
+                    try
+                    {
+                        client.Connect(ip);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Host: {0}:{1} is close", IpAddress, port);
+                        Console.WriteLine(ex);
+                        Environment.Exit(0);
+                    }
+                    Console.WriteLine("Host: {0}:{1} is open", IpAddress, port);
+                    break;
+                case "UDP":
+
+                    break;
+            }
         }
     }
 }
