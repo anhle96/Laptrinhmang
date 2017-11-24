7007e323c14ec36f7d0ed73b8fb70266dcf4ad5a
unknown <lequocanh96@gmail.com>
2017-11-24 10:45:41 +0700

    Update buoi 8 bai 2

--- LeQuocAnh-b8/Chuong_Trinh_Broadcast/Broadcast_Nhan/Program.cs
+++ LeQuocAnh-b8/Chuong_Trinh_Broadcast/Broadcast_Nhan/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Broadcast_Nhan
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-        }
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 9050);
+            sock.Bind(iep);
+            EndPoint ep = (EndPoint)iep;
+            Console.WriteLine("Readt to recieve...");
+            byte[] data = new byte[1024];
+            int recv = sock.ReceiveFrom(data, ref ep);
+            string stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine("Received: {0} from {1}", stringData, ep.ToString());
+            data = new byte[1024];
+            recv = sock.EndReceiveFrom(data, ref ep);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine("recevied: {0} from: {1}", stringData, ep.ToString());
+            sock.Close()
+;        }
     }
 }
---
+++ LeQuocAnh-b8/Chuong_trinh_Multicast/Multicast_nhan/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Multicast_nhan
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            Console.WriteLine("Ready to Recieve..");
+            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 9050);
+            EndPoint ep = (Endpoint)iep;
+            sock.Bind(iep);
+            sock.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(IPAddress.Parse("224.100.0.1")));
+            byte[] data = new byte[1024];
+            int recv = sock.EndReceiveFrom(data, ref ep);
+            string stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.WriteLine("recevied: {0} from : {1}", stringData, ep.ToString());
+            sock.Close();
+        }
+    }
+}
