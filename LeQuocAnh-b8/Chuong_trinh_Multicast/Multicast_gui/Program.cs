87daeaf52b410656c1d158210d32c6f53fd7c74a
unknown <lequocanh96@gmail.com>
2017-12-14 07:43:34 +0700

    Update buoi 2 và 6

--- LeQuocAnh-b8/Chuong_trinh_Multicast/Multicast_gui/Program.cs
+++ LeQuocAnh-b8/Chuong_trinh_Multicast/Multicast_gui/Program.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace Multicast_gui
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("224.100.0.1"), 9050);
+            byte[] data = Encoding.ASCII.GetBytes("This is a test message");
+            server.SendTo(data, iep);
+            server.Close();
         }
     }
 }
